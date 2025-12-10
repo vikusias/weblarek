9e0dccd2fa8479f5fafc9ca58bb50608f7a921fa
Victoria Shumskaya <shumvikashum@gmail.com>
2025-12-10 20:28:23 +0300

    Изменение файла

--- src/components/View/CardPreviewView.ts
+++ src/components/View/CardPreviewView.ts
@@ -1,54 +1,93 @@
 import { CardView } from "./CardView";
 import { IProduct, ICardActions } from "../../types";
+import { categoryMap } from "../../utils/constants";
 
-interface ICardPreview extends IProduct {
-  buttonText: string; // Текст кнопки
-  canBuy: boolean; // Можно ли купить
-  description: string; // Описание
-}
-
-// Вью для предпросмотра карточки
-export class CardPreviewView extends CardView<ICardPreview> {
-  protected _description: HTMLElement; // Элемент описания
-  protected _button: HTMLButtonElement; // Кнопка
+export class CardPreviewView extends CardView<IProduct> {
+  private categoryElement: HTMLElement | null = null;
+  private imageElement: HTMLImageElement | null = null;
+  private descriptionElement: HTMLElement | null = null;
+  private buttonElement: HTMLButtonElement | null = null;
 
   constructor(container: HTMLElement, actions?: ICardActions) {
     super(container);
 
-    this._description = this.element.querySelector(".card__text")!;
-    this._button = this.element.querySelector(".button")!;
+    // Находим элементы, специфичные для превью
+    this.categoryElement = this.container.querySelector(".card__category");
+    this.imageElement = this.container.querySelector(".card__image");
+    this.descriptionElement = this.container.querySelector(".card__text");
+    this.buttonElement = this.container.querySelector(".card__button");
 
-    if (actions?.onClick) {
-      this._button.addEventListener("click", actions.onClick);
+    // Обработка клика по кнопке
+    if (actions?.onClick && this.buttonElement) {
+      this.buttonElement.addEventListener("click", actions.onClick);
     }
   }
 
-  // Установка описания
+  // Специфичные для превью свойства
+  set category(value: string) {
+    if (this.categoryElement) {
+      this.setText(this.categoryElement, value);
+
+      const categoryKey = value as keyof typeof categoryMap;
+      const className = categoryMap[categoryKey] || "card__category_other";
+      this.categoryElement.className = `card__category ${className}`;
+    }
+  }
+
+  set image(value: string) {
+    if (this.imageElement) {
+      this.setImage(this.imageElement, value, "Изображение товара");
+    }
+  }
+
   set description(value: string) {
-    this.setText(this._description, value);
+    if (this.descriptionElement) {
+      this.setText(this.descriptionElement, value);
+    }
   }
 
-  // Установка возможности покупки
-  set canBuy(value: boolean) {
-    this.buttonDisabled = !value;
+  set buttonText(value: string) {
+    if (this.buttonElement) {
+      this.setText(this.buttonElement, value);
+    }
   }
 
-  // Метод рендеринга
-  render(data: Partial<ICardPreview>): HTMLElement {
+  set buttonDisabled(value: boolean) {
+    if (this.buttonElement) {
+      this.setDisabled(this.buttonElement, value);
+    }
+  }
+
+  render(
+    data: Partial<IProduct> & {
+      description?: string;
+      buttonText?: string;
+      buttonDisabled?: boolean;
+    }
+  ): HTMLElement {
     super.render(data);
 
-    if (data.description) {
+    if (data.category && this.categoryElement) {
+      this.category = data.category;
+    }
+
+    if (data.image && this.imageElement) {
+      this.image = data.image;
+    }
+
+    if (data.description && this.descriptionElement) {
       this.description = data.description;
     }
 
-    if (data.buttonText) {
+    if (data.buttonText && this.buttonElement) {
       this.buttonText = data.buttonText;
     }
 
-    if (data.canBuy !== undefined) {
-      this.canBuy = data.canBuy;
+    if (data.buttonDisabled !== undefined && this.buttonElement) {
+      this.buttonDisabled = data.buttonDisabled;
     }
 
-    return this.element;
+    return this.container;
   }
 }
+
