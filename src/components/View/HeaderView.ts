500139a7b301ea5128a778f531f3c6b0e88d856d
Victoria Shumskaya <shumvikashum@gmail.com>
2025-12-10 20:26:12 +0300

    Обновленный файл

--- src/components/View/HeaderView.ts
+++ src/components/View/HeaderView.ts
@@ -5,24 +5,25 @@ import { IEvents } from "../base/Events";
 interface IHeaderData {
   count: number; // Количество товаров в корзине
 }
+
 // Вью для шапки сайта
 export class HeaderView extends Component<IHeaderData> {
-  protected _counter: HTMLElement; // Элемент для отображения количества
-  protected _basket: HTMLButtonElement; // Кнопка корзины
+  protected counter: HTMLElement;
+  protected basket: HTMLButtonElement;
 
   constructor(container: HTMLElement, events: IEvents) {
     super(container);
 
-    this._counter = this.element.querySelector(".header__basket-counter")!;
-    this._basket = this.element.querySelector(".header__basket")!;
+    this.counter = this.container.querySelector(".header__basket-counter")!;
+    this.basket = this.container.querySelector(".header__basket")!;
 
-    this._basket.addEventListener("click", () => {
-      events.emit("basket:open"); // Эмиттинг события открытия корзины
+    this.basket.addEventListener("click", () => {
+      events.emit("basket:open");
     });
   }
 
   // Обновление количества товаров
   set count(value: number) {
-    this.setText(this._counter, value.toString());
+    this.setText(this.counter, value.toString());
   }
 }
