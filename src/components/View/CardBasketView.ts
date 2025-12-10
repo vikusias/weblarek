97f4dfb83cbc4c7ea6669b911a918ad6cd19f633
Victoria Shumskaya <shumvikashum@gmail.com>
2025-12-10 20:29:21 +0300

    Изменение файла

--- src/components/View/CardBasketView.ts
+++ src/components/View/CardBasketView.ts
@@ -1,38 +1,37 @@
 import { CardView } from "./CardView";
 import { IProduct, ICardActions } from "../../types";
 
-// Расширение интерфейса товара для корзины
-interface ICardBasket extends IProduct {
-  index: number; // индекс позиции в корзине
-}
-
-// Вью для карточки товара в корзине
-export class CardBasketView extends CardView<ICardBasket> {
-  protected _index: HTMLElement; // элемент для отображения индекса
-  protected _deleteButton: HTMLButtonElement; // кнопка удаления
+export class CardBasketView extends CardView<IProduct> {
+  private indexElement: HTMLElement | null = null;
+  private deleteButton: HTMLButtonElement | null = null;
 
   constructor(container: HTMLElement, actions?: ICardActions) {
     super(container);
 
-    this._index = this.element.querySelector(".basket__item-index")!;
-    this._deleteButton = this.element.querySelector(".basket__item-delete")!;
+    // Находим элементы, специфичные для карточки в корзине
+    this.indexElement = this.container.querySelector(".basket__item-index");
+    this.deleteButton = this.container.querySelector(".basket__item-delete");
 
-    if (actions?.onClick) {
-      this._deleteButton.addEventListener("click", actions.onClick);
+    // Обработка клика по кнопке удаления
+    if (actions?.onClick && this.deleteButton) {
+      this.deleteButton.addEventListener("click", actions.onClick);
     }
   }
-  // Установка индекса
+
+  // Специфичные для корзины свойства
   set index(value: number) {
-    this.setText(this._index, value.toString());
+    if (this.indexElement) {
+      this.setText(this.indexElement, String(value));
+    }
   }
-  // Рендеринг
-  render(data: Partial<ICardBasket>): HTMLElement {
+
+  render(data: Partial<IProduct> & { index?: number }): HTMLElement {
     super.render(data);
 
-    if (data.index !== undefined) {
+    if (data.index !== undefined && this.indexElement) {
       this.index = data.index;
     }
 
-    return this.element;
+    return this.container;
   }
 }
