851e58fbae571669bee0b078f5e6558cf7305077
Victoria Shumskaya <shumvikashum@gmail.com>
2025-12-10 19:57:30 +0300

    Изменение в main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,19 +1,27 @@
 import "./scss/styles.scss";
-
 import { EventEmitter } from "./components/base/Events";
 import { Api } from "./components/base/Api";
 import { ProductApi } from "./components/ProductApi";
 import { App } from "./components/App";
 import { API_URL } from "./utils/constants";
 
-// Инициализация
+import { Catalog } from "./components/Models/Catalog";
+import { Basket } from "./components/Models/Basket";
+import { Customer } from "./components/Models/Customer";
+
 const events = new EventEmitter();
 const api = new Api(API_URL);
 const productApi = new ProductApi(api);
 
-// Создание приложения
+// Создание моделей данных
+const catalog = new Catalog(events);
+const basket = new Basket(events);
+const customer = new Customer(events);
+
+// Создание и инициализация приложения
 try {
-  new App(events, productApi);
+  // Создаем объект App без присвоения переменной
+  new App(events, productApi, catalog, basket, customer);
   console.log("Приложение Web-Ларёк запущено!");
 } catch (error) {
   console.error("Ошибка при запуске приложения:", error);
