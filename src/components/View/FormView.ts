51d66f139dbc56dfdc8c6b3de51454d1f6e0532a
Victoria Shumskaya <shumvikashum@gmail.com>
2025-12-10 20:34:51 +0300

    Create FormView.ts

---
+++ src/components/View/FormView.ts
@@ -0,0 +1,35 @@
+import { Component } from "../base/Component";
+import { IEvents } from "../base/Events";
+
+export abstract class FormView<T> extends Component<T> {
+  protected submitButton: HTMLButtonElement;
+  protected errorsElement: HTMLElement;
+
+  constructor(container: HTMLElement, protected events: IEvents) {
+    super(container);
+
+    this.submitButton = this.container.querySelector('button[type="submit"]')!;
+    this.errorsElement = this.container.querySelector(".form__errors")!;
+
+    this.container.addEventListener("submit", (e) => {
+      e.preventDefault();
+      this.handleSubmit();
+    });
+  }
+
+  protected abstract handleSubmit(): void;
+
+  set valid(value: boolean) {
+    this.setDisabled(this.submitButton, !value);
+  }
+
+  set errors(value: string[]) {
+    const text = value.join(", ");
+    this.setText(this.errorsElement, text);
+    this.toggleClass(
+      this.errorsElement,
+      "form__errors_visible",
+      text.length > 0
+    );
+  }
+}
